dbf13ff65ed71c1842e7ec9ca3ae2e264e7d3f03
rockdaworld <sjh002@ucsd.edu>
2023-05-16 17:52:32 -0700

    "dict of names"

--- backend/app/dictnames.js
+++ backend/app/dictnames.js
@@ -1,9 +1,9 @@
 // dictionary of people in geisel.
 // key value is the UUID/GUID, value is the name of the person
-const people_in_geisel = {};
+export const people_in_geisel = {};
 
 // to insert into dictionary, use this function with a string argument
-function add_to_geisel_list(string_name) {
+export function add_to_geisel_list(string_name) {
     let uuid = self.crypto.randomUUID();
     people_in_geisel[string_name] = uuid;
 }
